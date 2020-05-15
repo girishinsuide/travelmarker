bf9eb8b44fdf7ff00603523acb277b2aa2f09089
girish <girish.h@insuide.com>
2020-05-15 17:53:19 +0530

    mapurl

--- src/app/mapurl/mapurl.component.ts
+++ src/app/mapurl/mapurl.component.ts
@@ -20,61 +20,89 @@ export class MapurlComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-  	this.initialize();
+    this.initMap();
+  	
   }
 
-  initialize() {
-    var latlng = new google.maps.LatLng(25.321824, 55.520725);
-    var mapOpts = {
-        center: latlng,
-        zoom: 15,
-        maxZoom:20,
-        minZoom:14,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
+ markers = [];
+ 
+// First, create an object containing LatLng and population for each city.    
+ //citymap = {lat:21.002471054356725, lng:79.12353515625};  
+ citymap = {
+  chicago: {
+    center: new google.maps.LatLng(41.878113, -87.629798),
+    population: 200000
+  },
+  newyork: {
+    center: new google.maps.LatLng(40.714352, -74.005973),
+    population: 100000
+  },
   
-    this.map = new google.maps.Map(this.el.nativeElement,mapOpts);
+};
+    
+ initMap() {    
+      
+  this.map = new google.maps.Map(document.getElementById('map'), {    
+    zoom: 4,    
+    center: new google.maps.LatLng(37.09024, -95.712891),    
+    mapTypeId: google.maps.MapTypeId.TERRAIN    
+  }); 
+
+  for (var city in this.citymap) {
+    var populationOptions = {
+      strokeColor: '#FF0000',
+      strokeOpacity: 0,
+      strokeWeight: 2,
+      fillColor: '#FF0000',
+      fillOpacity: 0.35,
+      map: this.map,
+      draggable: true,
+      center: this.citymap[city].center,
+      radius: Math.sqrt(this.citymap[city].population) * 100
+    };
+    // Add the circle for this city to the map.
+    var cityCircle = new google.maps.Circle(populationOptions);
+    var marker = new google.maps.Marker({
+      position: this.citymap[city].center,
+      draggable: true,
+      map: this.map
+    });
+    marker.bindTo("position", cityCircle, "center");
+  }   
+
+  /*var marker = new google.maps.Marker({    
+    position: lat_lng,    
+    map: this.map    
+  });  */
+
+  // This event listener will call addMarker() when the map is clicked.    
+  marker.addListener('click', (event) => {    
+    //this.addCircle(event.latLng);    
+  }); 
+   google.maps.event.addListener(marker, 'dragend', ()=> {
+        console.log(this.citymap[city].population);
+        //console.log(this.citymap[city].LatLng);
+    });
 
 
-   /* this.layer = new google.maps.ImageMapType({
+} 
 
-        name: this.layerID,
-        getTileUrl: (coord, zoom) => {
-          console.log(coord);
-          //var url = this.TILE_URL;
-            
-          return ['http://mapwarper.net/maps/tile/40465/{z}/{x}/{y}.jpg'];
-        },
-        tileSize: new google.maps.Size(256, 256),
-        minZoom: 1,
-        maxZoom: 20
-   });
-    this.map.mapTypes.set(this.layerID, this.layer);
-      this.map.setMapTypeId(this.layerID);*/
-
-
-
-  var layer = new google.maps.ImageMapType({
-        //name: this.layerID,
-        getTileUrl: function(coord, zoom) {
-          console.log(coord);
-
-         /* var url = this.TILE_URL
-            .replace('{x}', coord.x)
-            .replace('{y}', coord.y)
-            .replace('{z}', zoom);*/
-          return 'http://mapwarper.net/maps/tile/28216/'+zoom+'/'+coord.x+'/'+coord.y+'.png';
-        },
-        tileSize: new google.maps.Size(256, 256),
-        //maxZoom: 15,
-        //minZoom: this.minzoom + 2,
-      });
-  this.map.overlayMapTypes.push(layer);
-
-     //this.map.mapTypes.set(this.layerID, layer);
-      //this.map.setMapTypeId(this.layerID);
-}
-
+   
+/* addCircle(location) {    
+  // Add the circle for this city to the map.    
+    var cityCircle = new google.maps.Circle({    
+      strokeColor: '#FF0000',    
+      strokeOpacity: 0.8,    
+      strokeWeight: 2,    
+      fillColor: '#FF0000',    
+      fillOpacity: 0.9,    
+      map: this.map,    
+      center: location,    
+      radius: 199999.45454,  
+      draggable:false  
+    });  
+} 
+*/
     
 }
 
